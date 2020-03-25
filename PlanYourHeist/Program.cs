89200545e57450d3618b3e5e9cc47fa6c697c48d
Connor Sullivan <cksullvn@gmail.com>
2020-03-24 20:57:33 -0500

    Phase 1 WIP. Basic functionality works but still working on validation of user inputs

--- PlanYourHeist/Program.cs
+++ PlanYourHeist/Program.cs
@@ -6,7 +6,43 @@ namespace PlanYourHeist
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Plan Your Heist!");
+
+            Console.WriteLine("Enter a team member's name");
+            var name = Console.ReadLine();
+            TeamMember newbie = new TeamMember(name);
+
+            Console.WriteLine("Enter the team member's skill level as an integer");
+            var skillInput = Console.ReadLine();
+            //if (!int.TryParse(skillInput, out _))
+            //{
+            //    Console.WriteLine("That's not a number. Please try again");
+
+            //}
+            //else
+            //{
+            //    newbie.SkillLevel = Convert.ToInt32(skillInput);
+            //}
+            newbie.SkillLevel = Convert.ToInt32(skillInput);
+
+            Console.WriteLine("Finally, enter the team member's courage factor as any decimal between 0 and 2");
+            var courage = decimal.Parse(Console.ReadLine());
+            if (courage < 2 && courage > 0)
+            {
+                newbie.CourageFactor = courage;
+                Console.WriteLine("Here is your new team member's info");
+                Console.WriteLine($"Name: {newbie.Name}");
+                Console.WriteLine($"Skill Level: {newbie.SkillLevel}");
+                Console.WriteLine($"Courage Factor: {newbie.CourageFactor}");
+            }
+            else
+            {
+                Console.WriteLine("Woah woah woah, I said between 0 and 2 please. Please start again.");
+            }
+
+
+
+
         }
     }
 }
---
+++ PlanYourHeist/TeamMember.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlanYourHeist
+{
+
+    class TeamMember
+    {
+        public string Name { get; set; }
+
+        int _skillLevel;
+        public int SkillLevel 
+        {
+            get
+            {
+                return this._skillLevel;
+            }
+
+            set
+            {
+                this._skillLevel = Math.Abs(value);
+            }
+        }
+
+        decimal _courageFactor;
+        public decimal CourageFactor 
+        {
+            get
+            {
+                return this._courageFactor;
+            }
+
+
+            set 
+            {
+                this._courageFactor = Math.Round(value, 1);
+            }
+
+        }
+
+        public TeamMember (string name)
+        {
+            Name = name;
+        }
+    }
+}
